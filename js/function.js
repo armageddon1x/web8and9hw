eccd139faeb4e28ef2b6d31392112c5d9ffba268
armageddon <armageddon1x@gmail.com>
2015-09-13 21:04:46 -0400

    added nav bar

--- js/function.js
+++ js/function.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	// variables for the time
 	var counter=0;
 	var dt=new Date();
 	var h=dt.getHours();
@@ -8,9 +9,12 @@ $(document).ready(function(){
 	var num;
 	var seconds_counter=0;
 	var tick;
+
+	// sound for clock
 	tick=document.createElement('audio');
  	tick.setAttribute('src','tick.mp3');
- 	$("#hr").css("background","black");
+
+ 		$("#hr").css("background","black");
 		$("#hr").css("color","white");
 		$("#min").css("background","black");
 		$("#min").css("color","white");
@@ -19,17 +23,17 @@ $(document).ready(function(){
 		$("#ms").css("background","black");
 		$("#ms").css("color","white");
 
+	//this tells the time 
 	var time_out=setInterval(function(){
-		//clearInterval(timer);
-		//console.log("time's up");
 		timegiver();
 		counterHandler();
 	},1000);
-
+	//this is for the milliseconds
 	var time_ms=setInterval(function(){
 		MS();
-	},100);
+	},250);
 
+	//main time function
 	function timegiver(){
 		dt=new Date();
 		h=dt.getHours();
@@ -39,22 +43,24 @@ $(document).ready(function(){
 		console.log(h);
 		console.log(m);
 		console.log(s);
-		$("#hr").html(fix(AM_PM(h)));
-		$("#min").html(fix(m));
-		$("#sec").html(fix(s));
+		$("#h").html(fix(AM_PM(h)));
+		$("#m").html(fix(m));
+		$("#s").html(fix(s));
 		color_swap_s();
 		color_swap_m();
 		color_swap_h();
 		tick.play();
 	};
 
+	//adds milliseconds
 	function MS(){
 		dt=new Date();
 		ms=dt.getMilliseconds();
-		$("#milli").html(ms);
+		$("#milli").html(fix_MS(ms));
 		color_swap_ms();
 	}
 
+	//alternates colors for the dots
 	function counterHandler(){
 		if(s%2===0){
 			seconds_counter++;
@@ -67,6 +73,7 @@ $(document).ready(function(){
 		}
 	};
 
+	//changes color for seconds
 	function color_swap_s(){
 		//defining colors for the background
 		r=Math.floor((Math.random()*255));
@@ -80,11 +87,13 @@ $(document).ready(function(){
 		color="rgb("+r+","+g+","+b+")";
 		word="rgb("+rc+","+gc+","+bc+")";
 		//color changer
+		$("#s").fadeOut(200);
+		$("#s").fadeIn(200);
 		$("#sec").css("background",color);
-		$("#sec").css("color",word);
-		$("#sec_s").fadeOut(100);
-		$("#sec_s").fadeIn(100);
+		$("#sec").css("color",word);	
 	}
+
+	//changes color for minutes
 	function color_swap_m(){
 		if (s===0) {
 			//defining colors for the background
@@ -99,10 +108,14 @@ $(document).ready(function(){
 			color="rgb("+r+","+g+","+b+")";
 			word="rgb("+rc+","+gc+","+bc+")";
 			//color changer
+			$("#m").fadeOut(200);
+			$("#m").fadeIn(200);
 			$("#min").css("background",color);
 			$("#min").css("color",word);
 		};
 	}
+
+	//changes color for hours
 	function color_swap_h(){
 		if (m===0) {
 				//defining colors for the background
@@ -117,11 +130,14 @@ $(document).ready(function(){
 			color="rgb("+r+","+g+","+b+")";
 			word="rgb("+rc+","+gc+","+bc+")";
 			//color changer
+			$("#h").fadeOut(200);
+			$("#h").fadeIn(200);
 			$("#hr").css("background",color);
 			$("#hr").css("color",word);
 		};
 	}
 
+		//changes color for milliseconds
 		function color_swap_ms(){
 				//defining colors for the background
 			r=Math.floor((Math.random()*255));
@@ -139,12 +155,25 @@ $(document).ready(function(){
 			$("#milli").css("color",word);
 	}
 
+	//makes numbers display without gaps in case of single digits or 
+	//double digits
 	function fix(num){
 		if(num<10){
 			num="0"+num;
 		};
 		return num;
 	};
+	function fix_MS(num){
+		if (num<100&&num>10){
+			num="0"+num
+		};
+		if (num<100&&num<10){
+			num="00"+num
+		};
+		return num;
+	}
+
+	//changes AM and PM
 	function AM_PM(num){
 		if(num>12){
 			num=num-12;
@@ -153,5 +182,19 @@ $(document).ready(function(){
 		};
 		return num;
 		};
+
+		$("li").hover(
+			function(){
+			$(this).css("background", "pink")
+			$(this).css("color", "purple")
+			},
+			function(){
+			$(this).css("background", "black")
+			$(this).css("color", "green")
+		});
+
+		$("li").click(function(){
+			$(this).css("border-radius", "40px")
+		});
 	});
 
